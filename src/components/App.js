efa6ed33ae399561a05d36681cd01c490a6a972d
clementmwamba <clementmwamba23@gmail.com>
2023-11-21 15:12:19 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+function App() {
+    const [data , getData] = useState(null)
+
+    useEffect (()=> {
+        
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then (response => response.json())
+        .then (data =>getData(data))
+
+    }, []);
+    console.log(data)
+
+  return (
+    <>
+        {data ? (
+        <img src={data.message} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+        
+    </>
+  )
+}
+
+export default App
